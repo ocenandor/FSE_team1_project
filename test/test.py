cc48fa06d23d0df99f0844293dc11d716a7d08cd
Artem Alekseev <alekseev.artem700@gmail.com>
2023-10-27 17:53:48 +0300

    Python model and first tests

---
+++ test/test.py
@@ -0,0 +1,13 @@
+from os import listdir
+from os.path import isfile, join
+
+test_path = '../Desktop/Project/test/img'
+imgs = [f for f in listdir(test_path) if isfile(join(test_path, f))]
+
+test_model = Model()
+
+for file_name in imgs:
+    digit = int(file_name.split('.png')[0])
+    prediciton = test_model.predict_digit(join(test_path, file_name))[0]
+    
+    assert digit == prediciton
