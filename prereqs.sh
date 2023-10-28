471422a1e2c7924af1d97ebcc2a42d9fb1e3f389
Kamil <kamil_20.03@mail.ru>
2023-10-28 16:03:36 +0300

    Edited prereqs.sh and added new requirements

--- prereqs.sh
+++ prereqs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt update
-apt install python3 python3-pip
+apt install python3 python3-pip g++ clang
 apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 pip install -r requirements.txt --break-system-packages
